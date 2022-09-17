a694a5b908d48a7a0b517a7aa7aa9eebe3604e4d
minizou <gh.mini.zou@gmail.com>
2022-09-17 08:55:08 -0500

    Add basic components on landing page, not organized

--- packages/frontend/pages/index.tsx
+++ packages/frontend/pages/index.tsx
@@ -1,12 +1,29 @@
 import type { NextPage } from "next";
-import { Box, Heading } from "@chakra-ui/react";
+import { Box, Text, Stack, Input, IconButton } from "@chakra-ui/react";
+import { ArrowForwardIcon } from "@chakra-ui/icons";
 import PageLayout from "../components/Layout/PageLayout";
+
+let access : boolean = false;
+
 
 const Home: NextPage = () => {
   return (
     <PageLayout title={"geese, by minihacks"}>
       <Box px={[5, 10]}>
-        <Heading as={"h1"}>geese</Heading>
+        <Text fontWeight='bold' fontSize="2xl">Phoned in Translation.</Text>
+        <Stack spacing={3}>
+        <Text>Your Preferred Language</Text>
+          <Input variant='filled' placeholder='Filled' />
+        <Text>Your Phone Number</Text>
+          <Input variant='filled' placeholder='Filled' />
+        </Stack>
+        <IconButton
+          colorScheme='teal'
+          aria-label='Call Segun'
+          isRound = {true}
+          size='lg'
+          icon={<ArrowForwardIcon />}
+        />
       </Box>
     </PageLayout>
   );
